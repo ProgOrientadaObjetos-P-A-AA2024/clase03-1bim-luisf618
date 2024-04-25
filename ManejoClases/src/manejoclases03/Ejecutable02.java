06f847251c73af660e3cc29f4b4b74f9527b802e
Luis Cordova <lcordovacarrion@gmail.com>
2024-04-25 15:39:49 -0500

    Clase 3

--- ManejoClases/src/manejoclases03/Ejecutable02.java
+++ ManejoClases/src/manejoclases03/Ejecutable02.java
@@ -20,7 +20,11 @@ public class Ejecutable02 {
         h1.establecerNombre("Vaca Ortíz");
         h2.establecerNombre("Militar");
         h3.establecerNombre("Manuel IM");
-        
+        /*
+        Esta imprimiendo el nombre asignado al atributo nombre del objeto h1,
+        tambien imprime los valores por defecto del resto de atributos al no
+        recibir ningun valor
+        */
         System.out.printf("%s - %d - %.2f\n", h1.obtenerNombre(),
                 h1.obtenerNumeroCamas(), h1.obtenerPresupuesto());
         
