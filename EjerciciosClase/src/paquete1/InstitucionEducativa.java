52c3af52ec9ca8b2961b5bd2a495ac05dd948a42
Luis Cordova <lcordovacarrion@gmail.com>
2024-04-25 16:54:55 -0500

    Clase 3

--- EjerciciosClase/src/paquete1/InstitucionEducativa.java
+++ EjerciciosClase/src/paquete1/InstitucionEducativa.java
@@ -10,11 +10,51 @@ package paquete1;
  * @author reroes
  */
 public class InstitucionEducativa {
+
     private String nombre;
     private String tipoInstitucion;
     private int numeroAlumnos;
     private int numeroDocentes;
     private int numeroSedes;
-    
+
     /*Crear los método necesarios*/
+    public void establecerNombre(String n) {
+        nombre = n;
+    }
+
+    public void establecerTipoInstitucion(String n) {
+        tipoInstitucion = n;
+    }
+
+    public void establecerNumeroALumnos(int a) {
+        numeroAlumnos = a;
+    }
+
+    public void establecerNumeroDocentes(int d) {
+        numeroDocentes = d;
+    }
+
+    public void establecerNumeroSedes(int s) {
+        numeroSedes = s;
+    }
+
+    public String obtenerNombre() {
+        return nombre;
+    }
+
+    public String obtenerTipoInstitucion() {
+        return tipoInstitucion;
+    }
+
+    public int obtenerNumeroAlumnos() {
+        return numeroAlumnos;
+    }
+
+    public int obtenerNumeroDocentes() {
+        return numeroDocentes;
+    }
+
+    public int obtenerNumeroSedes() {
+        return numeroSedes;
+    }
 }
--- EjerciciosClase/src/paquete2/Principal.java
+++ EjerciciosClase/src/paquete2/Principal.java
@@ -12,11 +12,27 @@ import paquete1.InstitucionEducativa;
  * @author reroes
  */
 public class Principal {
+
     public static void main(String[] args) {
         /*Un objeto de tipo InstitucionEducativa*/
-        
+
         InstitucionEducativa i1 = new InstitucionEducativa();
-        
-        // System.out.printf("%s     ", i1.);
+        String nombre = "UTPL";
+        String tipo = "Privada";
+        int numeroA = 3650;
+        int numeroD = 100;
+        int numeroS = 8;
+
+        i1.establecerNombre(nombre);
+        i1.establecerTipoInstitucion(tipo);
+        i1.establecerNumeroALumnos(numeroA);
+        i1.establecerNumeroDocentes(numeroD);
+        i1.establecerNumeroSedes(numeroS);
+
+        System.out.printf("Nombre de Institucion: %s\nTipo de Institucion: %s\n"
+                + "Numero de Alumnos: %d\nNumero de Docentes: %d\nNumero de "
+                + "Sedes: %d\n", i1.obtenerNombre(),
+                i1.obtenerTipoInstitucion(), i1.obtenerNumeroAlumnos(),
+                i1.obtenerNumeroDocentes(), i1.obtenerNumeroSedes());
     }
 }
